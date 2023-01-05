eaf2b211c856cf212442f33f494e1ef48d30868a
rickaed <[cedric.blesch@sncf.fr] (mailto:cedric.blesch@sncf.fr)>
2023-01-05 09:09:35 +0100

    first commit

---
+++ basic.ts
@@ -0,0 +1,21 @@
+// challenge.ts
+interface User {
+    name: string;
+    age: number;
+    birthday?: string;
+}
+
+const prettyPrintWilder = (users: User[]): void => {
+    users.map((user) => {
+        console.log(`${user.name} is ${user.age} years old`);
+    });
+};
+
+const wilders: User[] = [];
+const user1: User = { name: "Pierre", age: 23 };
+const user2: User = { name: "Paul", age: 32, birthday: "10/02/1990" };
+const user3: User = { name: "Jacques", age: 25 };
+wilders.push(user1);
+wilders.push(user2);
+wilders.push(user3);
+prettyPrintWilder(wilders);
